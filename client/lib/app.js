abb645f655a7c5cbbcee50d2d8da6d3ed7d11580
Daniel <truenorthtrainingcompany@gmail.com>
2015-12-29 15:09:44 -0500

    Step 11 Complete

--- client/lib/app.js
+++ client/lib/app.js
@@ -3,3 +3,14 @@
     'ui.router',
     'accounts.ui'
   ]);
+
+  function onReady() {
+  	angular.bootstrap(document, ['socially'], {
+  		strictDi: true
+  	});
+  }
+
+  if (Meteor .isCordova)
+  	angular.element(document).on("deviceready", onReady);
+  else
+  	angular.element(document).ready(onReady);
