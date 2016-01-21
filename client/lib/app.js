231d65a3dd268f07e63352bc10e794ea79cead14
Daniel <truenorthtrainingcompany@gmail.com>
2016-01-21 15:18:18 -0500

    Step 18 Complete How to work with ang md in angular-meteor app How to create custom Angular 1 forms for application's auth

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,19 +1,31 @@
-  angular.module('socially', [
-    'angular-meteor',
-    'ui.router',
-    'accounts.ui',
-    'angularUtils.directives.dirPagination',
-    'uiGmapgoogle-maps',
-    'ngMaterial'
-  ]);
+angular.module('socially', [
+  'angular-meteor',
+  'ui.router',
+  'accounts.ui',
+  'angularUtils.directives.dirPagination',
+  'uiGmapgoogle-maps',
+  'ngMaterial'
+]);
 
-  function onReady() {
-  	angular.bootstrap(document, ['socially'], {
-  		strictDi: true
-  	});
-  }
+angular.module('socially').config(($mdIconProvider) => {
+  $mdIconProvider
+    .iconSet("social", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-social.svg")
+    .iconSet("action", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-action.svg")
+    .iconSet("communication", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-communication.svg")
+    .iconSet("content", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-content.svg")
+    .iconSet("toggle", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-toggle.svg")
+    .iconSet("navigation", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-navigation.svg")
+    .iconSet("image", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-image.svg");
+});
 
-  if (Meteor .isCordova)
-  	angular.element(document).on("deviceready", onReady);
-  else
-  	angular.element(document).ready(onReady);
+
+function onReady() {
+	angular.bootstrap(document, ['socially'], {
+		strictDi: true
+	});
+}
+
+if (Meteor .isCordova)
+	angular.element(document).on("deviceready", onReady);
+else
+	angular.element(document).ready(onReady);
